2637e6bdf6285d16dff66d8580698e085165f5d0
tuanloc1105 <tuanloc1105@gmail.com>
2024-05-01 20:43:39 +0700

    update     >> remove unrelated error

--- service/constant/constant.go
+++ service/constant/constant.go
@@ -59,84 +59,28 @@ var (
 		ErrorCode:    -3,
 		ErrorMessage: "This url is configured method that not match with your current method, please check again (405 Method Not Allowed)",
 	}
-	QueryError = ErrorEnums{
-		ErrorCode:    1,
+	QueryStatementError = ErrorEnums{
+		ErrorCode:    -4,
 		ErrorMessage: "Query error",
 	}
-	CreateDuplicateUser = ErrorEnums{
-		ErrorCode:    2,
-		ErrorMessage: "User already exist",
-	}
 	JsonBindingError = ErrorEnums{
-		ErrorCode:    3,
+		ErrorCode:    -5,
 		ErrorMessage: "Json binding error",
 	}
 	AuthenticateFailure = ErrorEnums{
-		ErrorCode:    4,
+		ErrorCode:    -6,
 		ErrorMessage: "Authenticate fail",
 	}
 	Unauthorized = ErrorEnums{
-		ErrorCode:    5,
+		ErrorCode:    -7,
 		ErrorMessage: "Unauthorized",
 	}
 	DataFormatError = ErrorEnums{
-		ErrorCode:    6,
+		ErrorCode:    -8,
 		ErrorMessage: "Data format error",
 	}
-	UserNotExisted = ErrorEnums{
-		ErrorCode:    7,
-		ErrorMessage: "User not existed",
-	}
-	InvalidNumberOfUser = ErrorEnums{
-		ErrorCode:    8,
-		ErrorMessage: "The number of users in the same room must be greater than 2",
-	}
-	InvalidUserToPaidList = ErrorEnums{
-		ErrorCode:    9,
-		ErrorMessage: "The buyer must not be on the list of payers",
-	}
-	ExpenseDeleteNotSuccess = ErrorEnums{
-		ErrorCode:    10,
-		ErrorMessage: "An error occurred while deleting daily spending data",
-	}
-	ExpenseActiveNotSuccess = ErrorEnums{
-		ErrorCode:    11,
-		ErrorMessage: "An error occurred while activating daily spending data",
-	}
 	Forbidden = ErrorEnums{
-		ErrorCode:    12,
+		ErrorCode:    -9,
 		ErrorMessage: "You don't have permission to perform this action",
-	}
-	RoomHasBeenExisted = ErrorEnums{
-		ErrorCode:    13,
-		ErrorMessage: "Room has been existed",
-	}
-	RoomDoesNotExist = ErrorEnums{
-		ErrorCode:    14,
-		ErrorMessage: "Room does not exist",
-	}
-	RoomStillHavePeople = ErrorEnums{
-		ErrorCode:    15,
-		ErrorMessage: "The room you want to remove still have people live in there",
-	}
-	RoleNotExist = ErrorEnums{
-		ErrorCode:    16,
-		ErrorMessage: "Can not find role for user",
-	}
-	DeleteDefaultRoomError = ErrorEnums{
-		ErrorCode:    17,
-		ErrorMessage: "Can not delete default room",
-	}
-	ActionCannotPerformOnYourself = ErrorEnums{
-		ErrorCode:    18,
-		ErrorMessage: "You cannot perform this action to your self",
-	}
-	EmptyRoomError = ErrorEnums{
-		ErrorCode:    19,
-		ErrorMessage: "Room currenty does not have any memeber",
-	}
-	ChangeRoomForAllMemberError = ErrorEnums{
-		ErrorCode:    20,
-		ErrorMessage: "Change room for all member error",
 	}
 )
