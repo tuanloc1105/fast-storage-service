ce95eee6a29641ee7fb96c701a55a8bd98081d83
Minh Đức <70012356+ducminhle1904@users.noreply.github.com>
2024-05-27 22:10:13 +0700

    Dev (#10)
    * update: FE porting electron
    
    * update: Refreshing token
    
    * feat: integrate sotrage api
    
    * update: folder tree action menu
    
    * update: Nested folder menu, retrieve folder details
    
    * update: Big changes
    
    * update: Download file
    
    * test: Angular env
    
    * update: BACKEND_URL env

--- web/apps/fast-storage/src/app/core/interceptor/token.interceptor.ts
+++ web/apps/fast-storage/src/app/core/interceptor/token.interceptor.ts
@@ -13,7 +13,7 @@ export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
 
   if (token) {
     req = req.clone({
-      url: `${is_production ? '' : '/api'}${req.url}`,
+      url: `${is_production ? process.env['BACKEND_URL'] : '/api'}${req.url}`,
       setHeaders: {
         Authorization: `Bearer ${token}`,
         'ngrok-skip-browser-warning': 'pass',
@@ -21,7 +21,7 @@ export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
     });
   } else {
     req = req.clone({
-      url: `${is_production ? '' : '/api'}${req.url}`,
+      url: `${is_production ? process.env['BACKEND_URL'] : '/api'}${req.url}`,
       setHeaders: {
         'ngrok-skip-browser-warning': 'pass',
       },
