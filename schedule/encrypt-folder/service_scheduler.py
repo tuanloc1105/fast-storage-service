056a7c479974c6e6c83d39cc02610761ae49d081
tuanloc1105 <tuanloc1105@gmail.com>
2024-08-01 14:31:28 +0700

    add docker compose and relocate schedule folder

--- schedule/encrypt-folder/service_scheduler.py
+++ schedule/encrypt-folder/service_scheduler.py
@@ -5,28 +5,31 @@ import requests
 from datetime import datetime
 
 
+# Timeout in seconds
+timeout = 10  # Set the timeout to 10 seconds
+
+
 def call_fast_storage_api_to_encrypt_all_folder():
     now = datetime.now()
     time_formatted_time_vietnam_format = now.strftime("%d/%m/%Y %H:%M:%S.%f")[:-3]
     encrypt_folder_api_url = os.getenv("ENCRYPT_FOLDER_API_URL")
     encrypt_folder_api_key = os.getenv("ENCRYPT_FOLDER_API_KEY")
     print(f"Task is running at {time_formatted_time_vietnam_format}...")
-    request_payload: dict[str, any] = {
-        "request": {
-            "encrypt": True
-        }
-    }
-    request_header: dict[str, any] = {
-        "api-key": encrypt_folder_api_key
-    }
-    response: requests.Response = requests.post(encrypt_folder_api_url, headers=request_header, json=request_payload, verify=False)
-    print(f"    - Request URL: {encrypt_folder_api_url}")
-    print(f"    - Request Headers: {request_header}")
-    print(f"    - Request Data: {request_payload}")
+    request_payload: dict[str, any] = {"request": {"encrypt": True}}
+    request_header: dict[str, any] = {"api-key": encrypt_folder_api_key}
+    response: requests.Response = requests.post(
+        encrypt_folder_api_url,
+        headers=request_header,
+        json=request_payload,
+        verify=False,
+        timeout=timeout,
+    )
+    print(f"    - Request URL:          {encrypt_folder_api_url}")
+    print(f"    - Request Headers:      {request_header}")
+    print(f"    - Request Data:         {request_payload}")
     print(f"    - Response Status Code: {response.status_code}")
-    print(f"    - Response Headers: {response.headers}")
-    print(f"    - Response Text: {response.text}")
-
+    print(f"    - Response Headers:     {response.headers}")
+    print(f"    - Response Text:        {response.text}")
 
 
 # Schedule the task to run every hour
