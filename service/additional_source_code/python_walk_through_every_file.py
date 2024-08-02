b96fce6f20b4d46b43c456c2acb183bf60e08e91
tuanloc1105 <tuanloc1105@gmail.com>
2024-08-02 22:48:02 +0700

    fix python script

--- service/additional_source_code/python_walk_through_every_file.py
+++ service/additional_source_code/python_walk_through_every_file.py
@@ -7,9 +7,6 @@ if __name__ == "__main__":
         print('input path is not directory')
         sys.exit(1)
     for root, dirs, files in os.walk(input_path_of_directory):
-        if not dirs and not files:
-            print('empty directory')
-            sys.exit(1)
         for file in files:
             file_path = os.path.join(root, file)
             try:
